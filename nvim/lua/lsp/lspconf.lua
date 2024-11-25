5d6a07a0af3516dfc47cf3f1ab248fbd3ba4ea3c
Plamen Dinev <plamen_ven@abv.bg>
2024-11-25 10:37:32 +0200

    Add clang-format support for C and C++

--- nvim/lua/lsp/lspconf.lua
+++ nvim/lua/lsp/lspconf.lua
@@ -19,7 +19,7 @@ local on_attach = function(client, bufnr)
 --  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
 --  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
 --  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
---  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<localleader>tD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
   buf_set_keymap('n', '<localleader>lr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   buf_set_keymap('n', '<localleader>la', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
 --  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
@@ -28,6 +28,22 @@ local on_attach = function(client, bufnr)
 --  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
 --  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 --  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  buf_set_keymap('n', '<localleader>lds', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  vim.keymap.set('n', '<localleader>F', function()
+      -- format using clang-format
+      path = vim.fn.expand('%:p')
+      vim.api.nvim_buf_call(bufnr, function()
+        vim.api.nvim_command('write')
+      end)
+      local uv = require('luv')
+      local fsev = uv.new_fs_event()
+      fsev:start(path, {}, vim.schedule_wrap(function()
+        vim.api.nvim_command('checktime ' .. tostring(bufnr))
+        fsev:stop();
+      end))
+      io.popen('clang-format -i ' .. path .. ' 2> /dev/null'):close()
+    end, { buffer = bufnr})
+
 
 end
 
